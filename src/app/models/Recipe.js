ec70bb2fb3e73dcc8ce4b617d5115956b9c7a474
Andre Sampaio <andresampaiolm@gmail.com>
2020-11-24 11:54:53 -0300

    complementando commit anterior com os arquivo que nao haviam sido registrados

---
+++ src/app/models/Recipe.js
@@ -0,0 +1,173 @@
+const db = require('../../config/db')
+
+module.exports = {
+    home(callback) {
+        const query = `
+        SELECT recipes.*, chefs.name AS chef
+        FROM recipes
+        JOIN chefs
+        ON recipes.chef_id = chefs.id
+        LIMIT 6;
+        `
+
+        db.query(query, (err, results) => {
+            if (err) throw `Database error! ${err}`
+
+            callback(results.rows)
+        })
+    },
+    all() {
+        const query = `
+        SELECT recipes.*, chefs.name AS chef
+        FROM recipes
+        JOIN chefs
+        ON recipes.chef_id = chefs.id;
+        `
+
+        return db.query(query)
+    },
+    findBy(filter, callback) {
+        const query = `
+        SELECT recipes.*, chefs.name AS chef
+        FROM recipes
+        JOIN chefs
+        ON recipes.chef_id = chefs.id
+        WHERE recipes.title ILIKE '%${filter}%';
+        `
+
+        db.query(query, (err, results) => {
+            if (err) throw `Database error! ${err}`
+
+            callback(results.rows)
+        })
+    },
+    findByChef(chef_id) {
+        const query = `
+            SELECT * FROM recipes
+            WHERE chef_id = $1;
+        `
+
+        return db.query(query, [ chef_id ])
+    },
+    find(id) {
+        const query = `
+        SELECT recipes.*, chefs.name AS chef
+        FROM recipes
+        JOIN chefs
+        ON recipes.chef_id = chefs.id
+        WHERE recipes.id=$1;
+        `
+        
+        return db.query(query, [ id ])
+    },
+    findRecipeFiles(recipe_id) {
+        const query = `
+        SELECT files.*
+        FROM recipe_files
+        JOIN files
+        ON recipe_files.file_id = files.id
+        WHERE recipe_files.recipe_id = $1;
+        `
+
+        return db.query(query, [ recipe_id ])
+    },
+    create(data) {
+        const query = `
+        INSERT INTO recipes (
+            chef_id,
+            title,
+            ingredients,
+            preparation,
+            information,
+            created_at
+        ) VALUES ( $1, $2, $3, $4, $5, $6 )
+        RETURNING id;
+        `
+        
+        const values = [ 
+            data.chef_id, 
+            data.title, 
+            data.filteredIngredients, 
+            data.filteredPreparation, 
+            data.information, 
+            data.created_at,
+        ]
+        
+        return db.query(query, values)
+    },
+    update(data) {
+        const query = `
+        UPDATE recipes SET
+            chef_id=$1,
+            title=$2,
+            ingredients=$3,
+            preparation=$4,
+            information=$5
+        WHERE id=$6;
+        `
+
+        const values = [
+            data.chef_id,
+            data.title, 
+            data.filteredIngredients, 
+            data.filteredPreparation, 
+            data.information,
+            data.id
+        ]
+
+        return db.query(query, values)
+    },
+    delete(id) {
+        const query = `
+        DELETE FROM recipes 
+        WHERE id=$1;
+        `
+
+        return db.query(query, [ id ])
+    },
+    paginate(params) {
+        const { filter, limit, offset, callback } = params
+
+        let query = "",
+            filterQuery = "",
+            totalQuery = `(
+                SELECT COUNT(*) FROM recipes
+            ) AS total`
+
+        if (filter) {
+            filterQuery = `
+            WHERE recipes.title ILIKE '%${filter}%'
+            `
+
+            totalQuery = `(
+                SELECT COUNT(*) FROM recipes
+                ${filterQuery}
+            ) AS total`
+        }
+
+        query = `
+            SELECT recipes.*, ${totalQuery}, chefs.name AS chef
+            FROM recipes
+            JOIN chefs ON chefs.id = recipes.chef_id
+            ${filterQuery}
+            GROUP BY recipes.id, chefs.id
+            LIMIT $1 OFFSET $2;  
+        `
+
+        db.query(query, [ limit, offset ], (err, results) => {
+            if (err) throw `Database error! ${err}`
+
+            callback(results.rows)
+        })
+    },
+    recipeFileRelation({recipeId, fileId}) {
+        let query = `
+        INSERT INTO recipe_files (
+            recipe_id,
+            file_id
+        ) VALUES ($1, $2);
+        `
+        return db.query(query, [ recipeId, fileId ])
+    }
+
+}
